659df6ca33e3e17be971d99fe1fe466b8d5ce11f
kevo-1 <kevin.bastawrous@gmail.com>
2025-03-29 20:22:17 +0200

    added the trie implementation

---
+++ Trie.h
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <set>
+class Node {
+    public:
+    bool endWord;
+    Node* children[26];
+
+    Node() {
+        endWord = false;
+        for (int i = 0; i < 26; i++)
+        {
+            children[i] = nullptr;
+        }
+        
+    }
+};
+
+
+class Trie{
+    private:
+    Node* root;
+
+    public:
+    Trie():root(new Node()){};
+
+    void insertWord(std::string word) {
+        Node* dummy = root;
+        word = __sanitizeWord(word);
+        for (auto ch: word) {
+            if(dummy->children[ch-'a'] == nullptr) {
+                dummy->children[ch-'a'] = new Node();
+            }
+            dummy = dummy->children[ch-'a'];
+        }
+        dummy->endWord = true;
+    }
+
+    bool searchWord(std::string word) {
+        Node* dummy = root;
+        for (auto ch: word) {
+            if(dummy->children[ch-'a'] == nullptr) {
+                return false;
+            }
+            dummy = dummy->children[ch-'a'];
+        }
+        return dummy->endWord;
+    }
+
+    bool startsWith(std::string pre) {
+        Node* dummy = root;
+        for (auto ch: pre) {
+            if(dummy->children[ch-'a'] == nullptr) {
+                return false;
+            }
+            dummy = dummy->children[ch-'a'];
+        }
+        return true;
+    }
+
+    void deleteWord(std::string word) {
+        Node* dummy = root;
+        for (auto ch: word) {
+            if(dummy->children[ch-'a'] == nullptr) {
+                return;
+            }
+            dummy = dummy->children[ch-'a'];
+        }
+        if(dummy->endWord) {
+            dummy->endWord = false;
+        }
+    }
+
+    void displayTrie() {
+        __displayTrieHelper("", root);
+    }
+
+    private:
+    std::set<char> validChar = {'-', '_', '\'', '\"'};
+    void __displayTrieHelper(std::string pre, Node* node) {
+        if(node->endWord) {
+            std::cout<<pre<<'\n';
+        }
+        for (int i = 0; i < 26; i++) {
+            if (node->children[i] != nullptr) {
+                __displayTrieHelper(pre+char(i+'a'), node->children[i]);
+            }
+        }
+    }
+
+    std::string __sanitizeWord(std::string word) {
+        std::string res = "";
+        for (auto i : word) {
+            if(isalpha(i)) {
+                res += (islower(i)?i:tolower(i));
+            } else if (validChar.find(i) != validChar.end()) {
+                res += i;
+            }
+        }
+        return res;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
-#include "BST.h"
+// #include "BST.h"
+#include "Trie.h"
 
 int main(){
-    BST<int> tree;
+    // BST<int> tree;
+    // tree.Insert(50);
+    // tree.Insert(30);
+    // tree.Insert(70);
+    // tree.Insert(20);
+    // tree.Insert(40);
+    // tree.Insert(60);
+    // tree.Insert(80);
 
-    tree.Insert(50);
-    tree.Insert(30);
-    tree.Insert(70);
-    tree.Insert(20);
-    tree.Insert(40);
-    tree.Insert(60);
-    tree.Insert(80);
+    // std::cout << "InOrder Traversal: ";
+    // tree.DisplayTree(1); // Should print: 20 30 40 50 60 70 80
 
-    std::cout << "InOrder Traversal: ";
-    tree.DisplayTree(1); // Should print: 20 30 40 50 60 70 80
+    // std::cout << "\nPreOrder Traversal: ";
+    // tree.DisplayTree(2); // Should print: 50 30 20 40 70 60 80
 
-    std::cout << "\nPreOrder Traversal: ";
-    tree.DisplayTree(2); // Should print: 50 30 20 40 70 60 80
-
-    std::cout << "\nPostOrder Traversal: ";
-    tree.DisplayTree(3); // Should print: 20 40 30 60 80 70 50
+    // std::cout << "\nPostOrder Traversal: ";
+    // tree.DisplayTree(3); // Should print: 20 40 30 60 80 70 50
     
-    std::cout<< (tree.Search(50, tree.Root)?"50 is found":"50 is Not found") << '\n';
-    std::cout<< (tree.Search(90, tree.Root)?"90 is found":"90 is Not found") << '\n';
-    tree.Delete(tree.Root, 50);
-    tree.DisplayTree(2);
+    // std::cout<< (tree.Search(50, tree.Root)?"50 is found":"50 is Not found") << '\n';
+    // std::cout<< (tree.Search(90, tree.Root)?"90 is found":"90 is Not found") << '\n';
+    // tree.Delete(tree.Root, 50);
+    // tree.DisplayTree(2);
+    Trie tri;
+    tri.insertWord("cat");
+    tri.insertWord("car");
+    tri.insertWord("card");
+    tri.insertWord("carton");
+    tri.insertWord("cart");
+    tri.insertWord("cardi");
+    tri.insertWord("N");
+    tri.insertWord("Ni");
+    tri.insertWord("Nin");
+    tri.insertWord("Ninj");
+    tri.insertWord("Ninja");
+    tri.insertWord("Ninjb");
+    tri.insertWord("Ninjc");
+    tri.displayTrie();
     return 0;
 }
